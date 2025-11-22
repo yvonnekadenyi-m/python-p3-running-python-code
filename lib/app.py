1b2c7d3edb10885844720e9220e217f1c6a3244e
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-22 08:04:19 +0300

    added to app.py

---
+++ lib/app.py
@@ -0,0 +1,12 @@
+print ("Hello world!")
+print ("Hello sun!")
+print ("Hello sky!")
+
+print ("Hello world!", end="")
+print ("Hello sun!", end="!! ")
+print ("Hello sky!", end="!!!\n")
+
+print("Hello World! Pass this test, please.")
+
+
+
